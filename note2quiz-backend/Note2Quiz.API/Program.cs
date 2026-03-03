150d1740f756ae6eb8ec38452873b4dd584e26da
shakana0 <shakanajamal3@gmail.com>
2026-03-03 15:35:18 +0100

    feat: register AzureChatClient

--- note2quiz-backend/Note2Quiz.API/Program.cs
+++ note2quiz-backend/Note2Quiz.API/Program.cs
@@ -6,12 +6,31 @@ using Note2Quiz.API.Services;
 using Note2Quiz.API.Services.OpenAI;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSingleton<ImageAnalysisClient>(sp =>
 {
     var endPoint = builder.Configuration["AzureVision:Endpoint"];
     var key = builder.Configuration["AzureVision:Key"];
     return new ImageAnalysisClient(new Uri(endPoint), new AzureKeyCredential(key));
 });
+
+
+builder.Services.AddSingleton<IChatClient>(sp =>
+{
+    var endpoint = builder.Configuration["AzureOpenAI:Endpoint"]
+        ?? throw new InvalidOperationException("AzureOpenAI:Endpoint is missing in configuration.");
+
+    var apiKey = builder.Configuration["AzureOpenAI:ApiKey"]
+        ?? throw new InvalidOperationException("AzureOpenAI:ApiKey is missing in configuration.");
+
+    var deployment = builder.Configuration["AzureOpenAI:DeploymentName"]
+        ?? throw new InvalidOperationException("AzureOpenAI:DeploymentName is missing in configuration.");
+
+    return new AzureChatClient(endpoint, apiKey, deployment);
+});
+
+
+
 builder.Services.AddScoped<IVisionService, VisionService>();
 builder.Services.AddScoped<IOpenAIService, OpenAIService>();
 builder.Services.AddScoped<IQuizService, QuizService>();
