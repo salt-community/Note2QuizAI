27ef884c637b7e2ee8b1e53472f62b232f6929ff
Johan Janérs <johanjaners@gmail.com>
2026-03-05 15:32:25 +0100

    fix: update BASE_URL and API endpoint for quiz generation

--- note2quiz-ui/src/config/apiConfig.ts
+++ note2quiz-ui/src/config/apiConfig.ts
@@ -1,9 +1,9 @@
-const  BASE_URL = import.meta.env.BASE_URL;
+const  BASE_URL = import.meta.env.VITE_API_BASE_URL;
 if(!BASE_URL)
     throw new Error("Base URL not defined!");
 
 export const API_ENDPOINTS = {
     quiz:{
-        generate:`${BASE_URL}/api/quiz/create`,
+        generate:`${BASE_URL}/api/quiz`,
     }
 }
