774d0b6eba59062c6f878fa16b8622e026821539
Johan Janérs <johanjaners@gmail.com>
2026-03-05 15:50:34 +0100

    fix: update base url

--- note2quiz-ui/src/config/apiConfig.ts
+++ note2quiz-ui/src/config/apiConfig.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 if (!BASE_URL) throw new Error("Base URL not defined!");
 
 export const API_ENDPOINTS = {
